79deaad6d9639f7801551b9be384af1eb443842d
cadfrunze <marius1890@yahoo.com>
2022-11-22 12:22:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.bgcolor('black')
+screen.setup(width=1200, height=800)
+screen.listen()
+t1 = Turtle()
+
+
+
+
+
+
+screen.exitonclick()
